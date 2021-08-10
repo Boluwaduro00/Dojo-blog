3e8a445be2629a640768e68c37bc97e5ab15e682
Boluwaduro00 <adebajobolu@gmail.com>
2021-08-10 22:32:21 +0100

    NewComponent

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,31 @@
-const Home = () => {
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
-    const handleClick = () => {
-        console.log('hello, ninjas')
+const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body:'lorem ipsum...', author: 'mario', id: 1 },
+        {title: 'Welcome party', body:'lorem ipsum...', author: 'yoshi', id: 2 },
+        {title: 'Web dev top tips', body:'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
+    const [name, setName] = useState('mario');
+
+    const handleDelete = (id)=> {
+        const newBlogs = blogs.filter(blog => (blog.id !== id));
+        setBlogs(newBlogs);
     }
+
+    useEffect(() =>{
+        console.log("UseeEffect ran");
+        //console.log(blogs);
+    }, [name])
 
     return (  
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick= {() => setName('luiji') }>Change name</button>
+            <p>{name}</p>        
         </div>
-    );
+    );   
 }
- 
+  
 export default Home;
