ddd2f4b5b8722fab0fcf443de686b271235140e0
Boluwaduro00 <adebajobolu@gmail.com>
2021-08-31 11:25:02 +0100

    newCoponent

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,45 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body:'lorem ipsum...', author: 'mario', id: 1 },
-        {title: 'Welcome party', body:'lorem ipsum...', author: 'yoshi', id: 2 },
-        {title: 'Web dev top tips', body:'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-    const [name, setName] = useState('mario');
+    const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id)=> {
-        const newBlogs = blogs.filter(blog => (blog.id !== id));
-        setBlogs(newBlogs);
-    }
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    // const handleDelete = (id)=> {
+    //     const newBlogs = blogs.filter(blog => (blog.id !== id));
+    //     setBlogs(newBlogs);
+    // }
 
     useEffect(() =>{
-        console.log("UseeEffect ran");
-        //console.log(blogs);
-    }, [name])
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+            .then(res => {
+                if(!res.ok){
+                    throw Error('Could not fetch the data for that resourse!!!')
+                }
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            })
+
+        }, 2000);
+    }, []);
 
     return (  
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-            <button onClick= {() => setName('luiji') }>Change name</button>
-            <p>{name}</p>        
+            {error && <div>{error}</div> }
+            {isPending && <div>Loading....</div> }
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}  
         </div>
-    );   
+    );
 }
-  
+
 export default Home;
